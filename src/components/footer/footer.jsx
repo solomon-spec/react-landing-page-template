b19cbd6a430c9aefc2da8bb3c744eedc9589fb20
solomon abate <solomonabate18@gmail.com>
2023-07-20 06:10:32 +0300

    footer added

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,7 @@ function footer() {
     return (
         <footer>
             <span>&copy; all rights reserved</span>
-            <span><a href="github.com/solomon-spec">Follow me on github</a></span>
+            <span><a href="https://github.com/solomon-spec">Follow me on github</a></span>
         </footer>
     )
 }
